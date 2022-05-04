cf6204232b491320c025114b0f1201c041f1bcf5
jabin34 <isratjabinesha@gmail.com>
2022-05-04 22:19:38 +0900

    navbar done

--- warehouse-client/src/App.js
+++ warehouse-client/src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import {Routes,Route,} from "react-router-dom";
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
+import MyItem from './components/MyItem/MyItem';
+import Blogs from './components/Blogs/Blogs';
+import Header from './components/Header/Header';
+import ManageInventory from './components/ManageInventory/ManageInventory';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Header/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+        <Route  path="/home" element={<Home />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/registration" element={<Registration/>}/>
+        <Route path="/manage" element={<ManageInventory/>} />
+        <Route path="/myitem" element={<MyItem/>} />
+        <Route path="/blogs" element={<Blogs/>} />
+    </Routes>
     </div>
   );
 }
--- warehouse-client/src/components/Header/Header.js
+++ warehouse-client/src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Header.css';
-
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
-        <div>
-            this is header
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"  sticky="top" >
+        <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/" >Home</Nav.Link>
+            <Nav.Link as={Link} to="/blogs">Blog</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/myitem">MyItem</Nav.Link>
+            <Nav.Link as={Link} to="/manage">Manage Inventory</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
