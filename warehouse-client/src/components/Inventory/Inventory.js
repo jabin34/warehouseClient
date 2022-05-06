d1f1e3c7248ecd8e35e125de4615f33cd752a0ac
jabin34 <isratjabinesha@gmail.com>
2022-05-06 16:06:39 +0900

    manage inventory ui done

--- warehouse-client/src/components/Inventory/Inventory.js
+++ warehouse-client/src/components/Inventory/Inventory.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import useInventory from '../../hooks/useInventory';
-import './Inventory.css';
+import React from "react";
+import useInventory from "../../hooks/useInventory";
+import "./Inventory.css";
+import SingleInventory from "../SingleInventory/SingleInventory";
+import { Row } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-import SingleInventory from '../SingleInventory/SingleInventory';
-import { Row } from 'react-bootstrap';
 const Inventory = () => {
-    const[data] = useInventory();
-    return (
-        <div className=''>
-            <Row xs={1} md={3} className="g-4 " >
-            {data.map(item =>
-       <SingleInventory item={item}/>
-    
-)}</Row>
-        </div>
-    );
+  const [data] = useInventory();
+  console.log(data);
+  return (
+    <div className="">
+      <Row xs={1} md={3} className="g-4 ">
+        {data.map((item) => (
+          <SingleInventory item={item} />
+        ))}
+      </Row>
+     <div className="d-flex p-3 justify-content-center "> <Link to="/manage">show more</Link></div>
+    </div>
+  );
 };
 
 export default Inventory;
--- warehouse-client/src/components/ManageInventory/ManageInventory.js
+++ warehouse-client/src/components/ManageInventory/ManageInventory.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import './ManageInventory.css';
+import React from "react";
+import {OverlayTrigger, Table, Tooltip } from "react-bootstrap";
+import useInventory from "../../hooks/useInventory";
+import "./ManageInventory.css";
+import { FiPlusCircle, FiTrash2} from "react-icons/fi";
+
 const ManageInventory = () => {
-    return (
-        <div>
-             ManageInventory
-        </div>
-    );
+    const [data] = useInventory();
+    let i=0;
+  return (
+    <div className="container p-1">
+      <Table responsive bordered hover rounded>
+        <thead>
+          <tr className="">
+            <th>No</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Description</th>
+            <th>Supplier</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+            { 
+            data.map(item=><tr key={data._id}>
+            <td>{++i}</td>
+            <td>
+            <img roundedCircle className="rounded align-middle"
+            src={item.img} alt="img" width={30} height={30} /></td>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+            <td>{item.quantity}</td>
+            
+            <td>{ item.desc.length>15?item.desc.slice(0,15)+"...":item.desc }</td>
+            <td>{item.supplier}</td>
+            <td className="align-middle d-flex justify-content-around">
+              <span className="p-2  border p-1 rounded"> <FiPlusCircle /></span>  
+              <span className="text-danger border p-2 rounded" onClick={()=>{handleDelete()}}><FiTrash2/></span>
+            </td>
+          </tr>)}
+          
+         
+        </tbody>
+      </Table>
+     
+    </div>
+  );
 };
 
 export default ManageInventory;
