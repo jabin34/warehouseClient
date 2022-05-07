a76825808bfcd0d405e6a5665cd556a03190a4d7
jabin34 <isratjabinesha@gmail.com>
2022-05-08 03:32:38 +0900

    not found added

--- warehouse-client/src/components/NotFound/NotFound.js
+++ warehouse-client/src/components/NotFound/NotFound.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            opps!! Not Found
+        <div className='container-fluid mx-auto w-60 border rounded shadow-sm mt-5 '>
+         <div className='m-5 text-center p-5'> 
+             <h1>404</h1>
+          <h1>  OOPS!! NOT FOUND</h1></div>
         </div>
     );
 };
